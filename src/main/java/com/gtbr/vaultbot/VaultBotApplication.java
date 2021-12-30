790c047b0b6754a26c373386300b4cec68eda608
thalles.tavares <thalles.tavares@ifood.com.br>
2021-12-29 23:41:26 -0300

    initial commit

---
+++ src/main/java/com/gtbr/vaultbot/VaultBotApplication.java
@@ -0,0 +1,13 @@
+package com.gtbr.vaultbot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VaultBotApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VaultBotApplication.class, args);
+    }
+
+}
