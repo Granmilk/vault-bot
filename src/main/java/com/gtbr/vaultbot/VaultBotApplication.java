a3c2352edaf11e80f0fe08f6f890dc08d6c695d5
thalles.tavares <thalles.tavares@ifood.com.br>
2021-12-30 16:35:45 -0300

    initial config

--- src/main/java/com/gtbr/vaultbot/VaultBotApplication.java
+++ src/main/java/com/gtbr/vaultbot/VaultBotApplication.java
@@ -1,13 +1,26 @@
 package com.gtbr.vaultbot;
 
+import net.dv8tion.jda.api.JDA;
+import net.dv8tion.jda.api.JDABuilder;
+
+import javax.security.auth.login.LoginException;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class VaultBotApplication {
 
-    public static void main(String[] args) {
+    private static JDA jda;
+    private static final String DISCORD_TOKEN = System.getenv("DISCORD_TOKEN");
+
+    public static void main(String[] args) throws LoginException {
         SpringApplication.run(VaultBotApplication.class, args);
+        jda = JDABuilder.createDefault(DISCORD_TOKEN).build();
+    }
+
+    public static JDA getJda(){
+        return jda;
     }
 
 }
