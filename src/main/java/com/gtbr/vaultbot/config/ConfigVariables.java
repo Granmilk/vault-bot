c61f5e633b1eedf702ae8f8e907c004028878754
thalles.tavares <thalles.tavares@ifood.com.br>
2022-01-07 17:32:14 -0300

    implement flyway

---
+++ src/main/java/com/gtbr/vaultbot/config/ConfigVariables.java
@@ -0,0 +1,20 @@
+package com.gtbr.vaultbot.config;
+
+import lombok.Getter;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+@Component
+@Getter
+public class ConfigVariables {
+
+    @Value("${spring.datasource.url}")
+    private String dataBaseUrl;
+
+    @Value("${spring.datasource.username}")
+    private String dataBaseUser;
+
+    @Value("${spring.datasource.password}")
+    private String dataBasePassword;
+}
