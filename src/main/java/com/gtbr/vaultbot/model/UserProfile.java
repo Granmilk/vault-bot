5591819441c6260aab6cf3e09c3c7d76dec2a14f
jtomaz <jtomaz@minsait.com>
2022-01-07 23:46:23 -0300

    Entity, repository and service class created from UserProfile task.

---
+++ src/main/java/com/gtbr/vaultbot/model/UserProfile.java
@@ -0,0 +1,42 @@
+package com.gtbr.vaultbot.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Data
+@Entity
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Table
+public class UserProfile {
+
+    @Id
+    @Column
+    private String discordId;
+
+    @Column
+    private String name;
+
+    @Column
+    private String discordTag;
+
+    @Column
+    private String registeredAt;
+
+    @Column
+    private String generalPassword;
+
+    @Column
+    private Boolean isAdmin;
+
+    @Column
+    private String guildId;
+}
